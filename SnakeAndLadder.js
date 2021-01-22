f2b893b0b0752ce42112703c35d9a693f4b99415
pradippatil01 <mr.pradip01@gmail.com>
2021-01-22 21:09:23 +0530

    Start : SnakeAndLadder file created and display welcome message

---
+++ SnakeAndLadder.js
@@ -0,0 +1,9 @@
+/**************************************************************************
+*   Excecution : 1. default Node 
+*   Purpose    : Snake and Ladder using Javascript
+*   @description 
+*   @author    : Pradip R patil (BridgeLabz)
+*   @file      : SnakeAndLadder.js
+*   @version   : v15.6.0
+***************************************************************************/
+console.log('Code for Snake and Ladder');
