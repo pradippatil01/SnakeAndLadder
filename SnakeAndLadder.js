dce314158ff75f1d5b5a5cebef260561f5a26416
pradippatil01 <mr.pradip01@gmail.com>
2021-01-23 16:35:58 +0530

    Refactor : checkplayeroption method refactored used ifelse instead switch

--- SnakeAndLadder.js
+++ SnakeAndLadder.js
@@ -14,28 +14,16 @@ const PLAYER_TWO = 2;
 var player1_Position = 0, player2_position = 0;
 var diceRollCount = 0;
 var diceValue;
+playerChange = true;
 var position = START_POSITION;
 /**
  * dice roll and get diceValue
  */
 var rollDice = () => {
     diceValue = Math.floor(Math.random() * 6) + 1;
+    console.log('dice value', diceValue);
     diceRollCount++;
     return diceValue;
-}
-/** 
- * if ladder come logic 
- */
-var ladder = (diceResult) => {
-    position = position + diceResult;
-    return position;
-}
-/** 
- * if snake come logic
- */
-var snake = (diceResult) => {
-    position = position - diceResult;
-    return position;
 }
 /** 
  *  checkplayer option and call ladder snake
@@ -44,41 +32,65 @@ var snake = (diceResult) => {
  *  @param '2' for snake
  */
 checkPlayerOption = () => {
-    let option = Math.floor(Math.random() * 3);
-    switch (option) {
-        case 0:
-            position = position + 0;
-            console.log("no play");
-            break;
-        case 1:
-            ladder(rollDice());
-            console.log("woww ladder");
-            break;
-        case 2:
-            snake(rollDice());
-            console.log("oops snake ");
-            break;
+    let rollDiceValue = rollDice();
+    let optionsCheckValue = Math.floor(Math.random() * 3);
+    if (optionsCheckValue == 0) {
+        console.log('no play');
+        position = position + 0;
+    }
+    if (optionsCheckValue == 1) {
+        position = position - rollDiceValue;
+        console.log('after snake come ',position);
+    }
+    if (optionsCheckValue == 2) {
+        position = position + rollDiceValue;
+        console.log('after ladder come', position);
+    }
+    return position;
+}
+
+var twoPlayerPlay = () => {
+    if (playerChange == true) {
+        player1_Position = playRule();
+        position = player2_position;
+        playerChange = false;
+        console.log("Player 1 Position:-  " + player1_Position);
 
     }
-}
-/**
- * main method from execution start
- */
-var startPlay = () => {
-    while (position < END_POSITION) {
-        checkPlayerOption();
-        // check if position less than Start position(0)
-        if (position < START_POSITION) {
-            position = START_POSITION;
-        }
-        // check if position go above Win position(100)
-        if (position > END_POSITION) {
-            position = position - diceValue;
-        }
-        console.log('player position ', position);
+    if (!playerChange) {
+        player2_position = playRule();
+        position = player1_Position;
+        playerChange = true;
+        console.log("Player 2 Position:-  " + player2_position);
     }
-    // print how many time dice rolled
+}
+
+
+var playRule = () => {
+       checkPlayerOption()
+    if (position < START_POSITION) {
+        position = START_POSITION;
+    }
+    if (position > END_POSITION) {
+        position = position - diceValue;
+    }
+    console.log('after snake and ladder real position ', position);
+    return position;
+}
+
+
+var startGame = () => {
+    while (position < END_POSITION) {
+        twoPlayerPlay();
+    }
+    if (player1_Position == END_POSITION) {
+        console.log("Player One Wins  " + player1_Position);
+    }
+    else if (player2_position == END_POSITION) {
+        console.log("Player Two Wins  " + player2_position);
+    }
     console.log('for winnig dice rolled ' + diceRollCount + ' times');
 }
 
-startPlay();
+startGame();
+
