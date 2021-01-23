45e31c11b6205cb6b5e60aa151b5cfa8b18c6843
pradippatil01 <mr.pradip01@gmail.com>
2021-01-23 09:19:18 +0530

    Add : player option checked using random function and apply switch case

--- SnakeAndLadder.js
+++ SnakeAndLadder.js
@@ -7,13 +7,48 @@
 *   @version   : v15.6.0
 ***************************************************************************/
 console.log('code for Snake and Ladder');
-var playerPosition = 0;
+var startPosition  = 0;
 var number = 0;
+var currentPosition;
 
-rollDice = () => {
+
+var rollDice = () => {
     number = Math.floor(Math.random() * 6) + 1;
+    console.log('dice rolled and value returned ',number)
     return number;
 }
 
-var s = rollDice();
-console.log(s);
+var ladder = (diceResult) => {
+    let positon = startPosition  + diceResult;
+    return positon;
+};
+
+
+var snake = (diceResult) => {
+    let positon = startPosition  - diceResult;
+    return positon;
+};
+
+checkPlayerOption = (option) => {
+    switch (option) {
+        case 0:
+            currentPosition = playerPosition;
+            console.log("no play");
+            console.log("updated player positon - " + currentPosition);
+            break;
+        case 1:
+            currentPosition = ladder(rollDice());
+            console.log("woww ladder");
+            console.log("updated player positon - " + currentPosition);
+            break;
+        case 2:
+            currentPosition = snake(rollDice());
+            console.log("oops snake ");
+            console.log("updated player positon - " + currentPosition);
+            break;
+
+    }
+}
+
+let option = Math.floor(Math.random() * 3);
+checkPlayerOption(option);
